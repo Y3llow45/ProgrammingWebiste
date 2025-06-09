6f62a94ee66da2985766b6eb8c4a5237bacbfd62
Y3llow45 <hristovborislav45@gmail.com>
2025-06-09 17:38:26 +0300

    Update structure

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
+import HomePage from './components/HomePage/HomePage';
 import { useState } from 'react';
 
 function App() {
